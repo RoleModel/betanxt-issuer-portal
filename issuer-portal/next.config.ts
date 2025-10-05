6db7cf58509243b05ffe84e2731816d681ff73ce
Dallas Peters <dallas.peters@rolemodelsoftware.com>
2025-10-05 18:14:43 -0500

    Remove experimental.globalNotFound and logging config

--- issuer-portal/next.config.ts
+++ issuer-portal/next.config.ts
@@ -4,16 +4,8 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    globalNotFound: true,
-  },
   productionBrowserSourceMaps: true,
   reactStrictMode: false,
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
   transpilePackages: [
     '@rolemodel/betanxt-design-system',
     '@mui/x-data-grid',
