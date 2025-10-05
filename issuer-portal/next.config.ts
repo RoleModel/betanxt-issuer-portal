5ae4e58e224674407ee3cc162ee66381ae2d1801
Dallas Peters <dallas.peters@rolemodelsoftware.com>
2025-10-05 18:28:58 -0500

    Remove experimental.globalNotFound causing invalid routes

--- issuer-portal/next.config.ts
+++ issuer-portal/next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    globalNotFound: true,
-  },
   productionBrowserSourceMaps: true,
   reactStrictMode: false,
   transpilePackages: [
