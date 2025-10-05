fa2f45f7121171d3ab0b1704a0c31ed07b183e63
Dallas Peters <dallas.peters@rolemodelsoftware.com>
2025-10-05 18:40:14 -0500

    Remove Layout from global-not-found to fix prerender error

--- issuer-portal/app/global-not-found.tsx
+++ issuer-portal/app/global-not-found.tsx
@@ -9,7 +9,7 @@ import { Container, Link as MuiLink, Typography } from '@mui/material'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
 import InitColorSchemeScript from '@mui/material/InitColorSchemeScript'
 import type { } from '@mui/material/themeCssVarsAugmentation'
-import Layout from '@/components/Layout/Layout'
+
 import BetaNXTThemeProvider from '@/components/mui-styling/ThemeRegistry'
 
 const roboto = Roboto({
@@ -34,47 +34,45 @@ export default function RootLayout() {
         <InitColorSchemeScript attribute="class" />
         <AppRouterCacheProvider>
           <BetaNXTThemeProvider>
-            <Layout>
-              <Container
-                maxWidth="md"
+            <Container
+              maxWidth="md"
+              sx={{
+                p: 3,
+                position: 'relative',
+                height: '80vh',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Typography
+                variant="pageTitle"
+                data-error="404"
                 sx={{
-                  p: 3,
-                  position: 'relative',
-                  height: '80vh',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  justifyContent: 'center',
+                  fontSize: '76px',
+                  '&:before': {
+                    color: (theme) => `rgba(${theme.vars.palette.primary.mainChannel} / 0.2)`,
+                    content: 'attr(data-error)',
+                    fontSize: '40vw',
+                    fontWeight: 700,
+                    textAlign: 'center',
+                    left: '50%',
+                    position: 'absolute',
+                    top: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: '100%',
+                    zIndex: 0,
+                  },
                 }}
               >
-                <Typography
-                  variant="pageTitle"
-                  data-error="404"
-                  sx={{
-                    fontSize: '76px',
-                    '&:before': {
-                      color: (theme) => `rgba(${theme.vars.palette.primary.mainChannel} / 0.2)`,
-                      content: 'attr(data-error)',
-                      fontSize: '40vw',
-                      fontWeight: 700,
-                      textAlign: 'center',
-                      left: '50%',
-                      position: 'absolute',
-                      top: '50%',
-                      transform: 'translate(-50%, -50%)',
-                      width: '100%',
-                      zIndex: 0,
-                    },
-                  }}
-                >
-                  404
-                </Typography>
-                <Typography variant="pageTitle">Sorry, we can&apos;t find that page.</Typography>
-                <MuiLink component={Link} href="/" sx={{ zIndex: 1 }}>
-                  Return Home
-                </MuiLink>
-              </Container>
-            </Layout>
+                404
+              </Typography>
+              <Typography variant="pageTitle">Sorry, we can&apos;t find that page.</Typography>
+              <MuiLink component={Link} href="/" sx={{ zIndex: 1 }}>
+                Return Home
+              </MuiLink>
+            </Container>
           </BetaNXTThemeProvider>
         </AppRouterCacheProvider>
       </body>
