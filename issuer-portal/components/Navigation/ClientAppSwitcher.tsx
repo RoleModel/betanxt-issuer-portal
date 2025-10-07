60afefd1b66abe999494e7f59cbd6774c3265b69
Dallas Peters <dallas.peters@rolemodelsoftware.com>
2025-10-07 11:38:32 -0500

    debug: add logging to ClientAppSwitcher SwitchButton

--- issuer-portal/components/Navigation/ClientAppSwitcher.tsx
+++ issuer-portal/components/Navigation/ClientAppSwitcher.tsx
@@ -23,6 +23,12 @@ function SwitchButton() {
   const { data: session } = useSession()
   const open = Boolean(anchorEl)
 
+  console.log('SwitchButton Debug:', {
+    availableClientsCount: availableClients.length,
+    availableClients: availableClients.map(c => ({ ticker: c.ticker, name: c.company_name })),
+    currentClient: currentClient?.ticker,
+  })
+
   // Check if user has permission to switch clients
   const userType = session?.user?.type
   const isAuthBypassed = process.env.NEXT_PUBLIC_BYPASS_AUTH === 'true'
